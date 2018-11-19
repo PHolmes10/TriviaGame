6439d7c91347914cfc314d29c92638e904d1bf22
pholmes10 <pholmes10@gmail.com>
2018-11-18 23:23:03 -0600

    added some js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,95 @@
-var questions = [{
-    q: "Is the sky blue?",
-    a: "t"
-},
-{
-    q: "Is water wet?",
-    a: "t"
-},
-{
-    q: "is this a windows computer?",
-    a: "f"
-}];
+var questions = [
+    {
+        "question": "The next Playstation platform will be:",
+        "allAnswers": ["PS5 ", "PS4K ", "PSX ", "PS8K "],
+        "correctAnswer": "B"
+    },
+    {
+        "question": "The Sloth's scientific name, Bradypus, is Greek for:",
+        "allAnswers": ["Moving slowly", "Swiftly", "Slow feet", "Sleeping"],
+        "correctAnswer": "C"
+    },
+    {
+        "question": "Earth is how far away from the Sun:",
+        "allAnswers": ["92.96 Million Miles", "82.96 Million Miles", "102.96 Million Miles", "72.96 Million Miles"],
+        "correctAnswer": "A"
+    },
+    {
+        "question": "The first issue of National Geographic was issued in:",
+        "allAnswers": ["1901", "1874", "1888", "1926"],
+        "correctAnswer": "C"
+    },
+    {
+        "question": "It is approximitely how hot on the planet Venus:",
+        "allAnswers": ["262 Degrees C", "162 Degrees C", "462 Degrees C", "862 Degrees C"],
+        "correctAnswer": "C"
+    },
+    {
+        "question": "What type of star is the Sun:",
+        "allAnswers": ["White Dwarf", "Yellow Dwarf", "The sun is NOT a star", "Orange Dwarf"],
+        "correctAnswer": "B"
+    },
+    {
+        "question": "Approximitely how much of the ocean has been explored:",
+        "allAnswers": ["12%", "23%", "38%", "5%"],
+        "correctAnswer": "D"
+    },
+    {
+        "question": "Approximitely how long did it take for the Apollo missions to reach the moon:",
+        "allAnswers": ["36 hours", "24 hours", "72 hours", "96 hours"],
+        "correctAnswer": "C"
+    },
+    {
+        "question": "The world record for most spoons balanced on the face is:",
+        "allAnswers": ["45 spoons", "62 spoons", "31 spoons", "91 spoons"],
+        "correctAnswer": "C"
+    },
+    {
+        "question": "The world record for most socks put on one foot in one minute is:",
+        "allAnswers": ["12 socks", "28 socks", "48 socks", "67 socks"],
+        "correctAnswer": "C"
+    }];
 
-var scores = 0;
-var counter = 0;
 
-function displayQuestions() {
-    document.getElementById("question").textContent = questions[counter].q;
-}
 
-displayQuestions();
+var correct = 0
+var incorrect = 0
 
-document.onkeyup = function (event) {
-    if (event.key === "t" || event.key === "f") {
-        if (event.key === questions[counter].a) {
-            alert("correct");
-            scores++;
-            document.getElementById("score").textContent = scores;
-        } else {
-            alert("wrong");
-        }
-        // alert("You can play");
-        counter++;
-        if (counter === questions.length) {
-            alert("Game Over");
-        };
-        displayQuestions();
 
-    } else {
-        alert("enter the right key");
-    };
-};
+
+// starts the game when user clicks the start div
+$("#start").on("click", function () {
+
+    for (i = 0; i < questions.length; i++) {
+
+        var questionDiv = $("<div class='questions'>");
+        var answerDiv = $("<div class='answers'>");
+
+
+        // var letter;
+        // var txt = "";
+        // for (letter in questions[i].allAnswers[i]) {
+        //     txt += questions[i].allAnswers[letter] + radio;
+        // };
+
+
+        var radio = $("<input>");
+        radio.attr("type", "radio");
+        questions[i].allAnswers.push(radio);
+
+        questionDiv.html(questions[i].question);
+        answerDiv.html(questions[i].allAnswers);
+
+        (questionDiv).append(answerDiv);
+
+        $("#question-list").append(questionDiv);
+    }
+
+    // hides start button
+    $("#start").hide();
+    $("#done").text("Submit");
+});
+
+
+
+
